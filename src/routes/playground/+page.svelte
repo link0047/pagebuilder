e49afa0770f21ebdfc6af34d6ec10a0534638fd1
Jamaal Williams <link0047@gmail.com>
2025-10-02 14:05:13 -0400

    modify playground

--- src/routes/playground/+page.svelte
+++ src/routes/playground/+page.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+  const blocks = Array(250).fill(0);
+</script>
+
+<div class="blocks">
+  {#each blocks as block}
+    <div class="block">{block}</div>
+  {/each}
+</div>
 <div class="uikit-bottom-bar"></div>
 
 <style>
@@ -8,5 +17,25 @@
     border-top-left-radius: .5rem;
     border-top-right-radius: .5rem;
     bottom: 0;
+    background-color: #212121;
+  }
+
+  .blocks {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: .5rem;
+    width: 100%;
+    max-width: 1200px;
+    grid-area: preview;
+  }
+
+  .block {
+    display: flex;
+    background-color: #212121;
+    justify-content: center;
+    align-items: center;
+    aspect-ratio: 1 / 1;
+    color: #fff;
+    border-radius: .5rem;
   }
 </style>
